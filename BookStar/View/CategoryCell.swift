7f4223403397b7d1b9172fc2214719a32252f554
Rondrivera <Ron.drivera@gmail.com>
2021-08-18 11:43:49 -0400

    added check to prevent crash when no book data available

--- BookStar/View/CategoryCell.swift
+++ BookStar/View/CategoryCell.swift
@@ -20,15 +20,17 @@ class CategoryCell: UITableViewCell {
     }
 
     func configureCell(genre: String, books: [Book]) {
-        if let url1 = URL(string: "https" + books[0].smallThumbnail.dropFirst(4)) {
+        
+        
+        if books.count > 0, let  url1 = URL(string: "https" + books[0].smallThumbnail.dropFirst(4)) {
             categoryImg1.kf.setImage(with: url1)
         }
         
-        if let url2 = URL(string: "https" + books[1].smallThumbnail.dropFirst(4)) {
+        if books.count > 1, let url2 = URL(string: "https" + books[1].smallThumbnail.dropFirst(4)) {
             categoryImg2.kf.setImage(with: url2)
         }
         
-        if let url3 = URL(string: "https" + books[2].smallThumbnail.dropFirst(4)) {
+        if books.count > 2, let url3 = URL(string: "https" + books[2].smallThumbnail.dropFirst(4)) {
             categoryImg3.kf.setImage(with: url3)
         }
         
